3ad08a3ab3add8bc29cca21d37eda5f5f8bcf32e
BTharungoud <122255721+BTharungoud@users.noreply.github.com>
2023-05-29 19:53:13 +0530

    completed step-3&2

--- server/server.js
+++ server/server.js
@@ -9,6 +9,38 @@ app.get('/',(req,res)=>{
     res.send('sucessfully created server');
 });
 
+app.get('/i',(req,res)=>{
+    let obj=[
+        {
+            endPoint:'/i/google',
+            url: 'https://www.google.com'
+         },
+         {
+            endPoint:'/i/github',
+            url: 'https://www.github.com'
+         }
+    ];
+    res.send(obj);
+    
+})
+
+app.get('/i/google',(req,res)=>{
+    res.send('https://www.google.com');
+})
+
+
+app.get('/i/github',(req,res)=>{
+    res.send('https://www.github.com');
+})
+
+app.get('/r/google',(req,res)=>{
+    res.redirect('https://www.google.com');
+})
+
+app.get('/r/github',(req,res)=>{
+    res.send('https://www.github.com');
+})
+
 app.listen(port,()=>{
     console.log(`Server running at http://localhost:${port}`);
 })
